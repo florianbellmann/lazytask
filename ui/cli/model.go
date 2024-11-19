44d2b29bccfed28d1e82ac42e144a9597086aafb
florianbellmann <flojuesten@gmail.com>
2024-11-19 11:27:29 +0100

    extend model for text input and modes

--- ui/cli/model.go
+++ ui/cli/model.go
@@ -1,10 +1,23 @@
 package cli
 
 import (
+	"time"
+
 	bubbleTeaList "github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
 
 	"lazytask/application"
 	"lazytask/domain"
+)
+
+// Define view modes
+type viewMode int
+
+const (
+	listMode viewMode = iota // initial value 0
+	textInputMode
+	dateInputMode
 )
 
 type listItem struct {
@@ -24,9 +37,16 @@ func (l listItem) FilterValue() string {
 }
 
 type model struct {
-	// Data parts
+	// Cli content
+	inputText textinput.Model // TODO: rename me
 
-	lists       []domain.List
+	mode viewMode
+
+	// Data parts
+	lists []domain.List
+
+	activeList string
+
 	tasks       []domain.Task
 	taskService *application.TaskService
 
@@ -41,6 +61,10 @@ type model struct {
 	listModel bubbleTeaList.Model
 }
 
+func TextInputInit() tea.Cmd {
+	return textinput.Blink
+}
+
 func NewModel(taskService *application.TaskService) model {
 	// var (
 	// 	delegateKeys = newDelegateKeyMap()
@@ -50,14 +74,17 @@ func NewModel(taskService *application.TaskService) model {
 	uiList := bubbleTeaList.New([]bubbleTeaList.Item{}, bubbleTeaList.NewDefaultDelegate(), 0, 0)
 	// delegate := newItemDelegate(delegateKeys)
 	// myList := list.New(items, delegate, 0, 0)
-	uiList.Title = "Tasks"
+	uiList.Title = "develop"
+
 	uiList.Styles.Title = titleStyle
 	// myList.AdditionalFullHelpKeys = func() []key.Binding {
 	// 	return createKeyBindings(listKeys)
 	// }
 
 	return model{
+		mode:        listMode,
 		lists:       []domain.List{},
+		activeList:  "develop",
 		tasks:       []domain.Task{},
 		taskService: taskService,
 		listModel:   uiList,
@@ -76,9 +103,16 @@ func (m *model) LoadTasks(tasks []domain.Task) {
 
 	// log.Printf("Items: %v", items)
 
+	// TODO: load only overdue tasks. how does this work with the indexes?
+
 	items := []bubbleTeaList.Item{}
 	for _, task := range tasks {
-		items = append(items, listItem{task: task})
+		// build a time.Time object of today 23:59
+		now := time.Now()
+		endOfToday := time.Date(now.Year(), now.Month(), now.Day(), 23, 59, 59, 0, now.Location())
+		if !task.DueDate.IsZero() && task.DueDate.Before(endOfToday) {
+			items = append(items, listItem{task: task})
+		}
 	}
 	m.listModel.SetItems(items)
 }

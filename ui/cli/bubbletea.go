19f0e2d4847010b8fb0d7542a76ccf1a0226ac29
florianbellmann <flojuesten@gmail.com>
2024-11-19 11:26:52 +0100

    text input working

--- ui/cli/bubbletea.go
+++ ui/cli/bubbletea.go
@@ -1,10 +1,19 @@
 package cli
 
 import (
-	tea "github.com/charmbracelet/bubbletea"
+	"fmt"
 	"lazytask/application"
+	"lazytask/domain"
 	"log"
 	"os"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+// TODO: is this used?
+type (
+	errMsg error
 )
 
 type BubbleTeaApp struct {
@@ -17,54 +26,149 @@ func NewBubbleTeaApp(ts application.TaskService) *BubbleTeaApp {
 }
 
 func (m model) Init() tea.Cmd {
+	// switch m.mode {
+	// case listMode:
+	// 	return nil
+	// case textInputMode:
+	// 	return TextInputInit()
+	// }
 	return nil
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmds []tea.Cmd
+	switch m.mode {
+	case listMode:
+		var cmds []tea.Cmd
 
-	switch msg := msg.(type) {
-	case CompleteTaskMsg:
-		// h, v := appStyle.GetFrameSize()
-		// m.listModel.SetSize(msg.Width-h, msg.Height-v)
+		switch msg := msg.(type) {
+		case CompleteTaskMsg:
+			// h, v := appStyle.GetFrameSize()
+			// m.listModel.SetSize(msg.Width-h, msg.Height-v)
 
-		// Update the completed task in the Fancy List
-		for i, task := range m.tasks {
-			if task.Id == msg.taskID {
-				m.tasks[i].IsCompleted = true
+			// Update the completed task in the Fancy List
+			for i, task := range m.tasks {
+				if task.Id == msg.taskID {
+					m.tasks[i].IsCompleted = true
+				}
 			}
+
+			m.LoadTasks(m.tasks) // Reload tasks into the list
+
+		case tea.WindowSizeMsg:
+			h, v := appStyle.GetFrameSize()
+			m.listModel.SetSize(msg.Width-h, msg.Height-v)
+
+		case ErrorMsg:
+			m.err = msg.err
+
+		case tea.KeyMsg:
+			// // TODO: move this to input as well
+			// // Don't match any of the keys below if we're actively filtering.
+			// if m.list.FilterState() == list.Filtering {
+			// 	break
+			// }
+			//
+			// handleKeyMsg(m, msg)
+
+			// Handle key presses
+			return HandleKeyPress(m, msg)
+
 		}
 
-		m.LoadTasks(m.tasks) // Reload tasks into the list
+		// This will also call our delegate's update function.
+		newListModel, cmd := m.listModel.Update(msg)
+		m.listModel = newListModel
+		cmds = append(cmds, cmd)
 
-	case tea.WindowSizeMsg:
-		h, v := appStyle.GetFrameSize()
-		m.listModel.SetSize(msg.Width-h, msg.Height-v)
+		return m, tea.Batch(cmds...)
 
-	case ErrorMsg:
-		m.err = msg.err
+	case textInputMode:
+		var cmd tea.Cmd
 
-	case tea.KeyMsg:
-		// // TODO: move this to input as well
-		// // Don't match any of the keys below if we're actively filtering.
-		// if m.list.FilterState() == list.Filtering {
-		// 	break
-		// }
-		//
-		// handleKeyMsg(m, msg)
+		switch msg := msg.(type) {
+		case tea.KeyMsg:
+			switch msg.Type {
+			case tea.KeyCtrlC, tea.KeyEsc:
+				m.inputText.Reset()
+				m.mode = listMode
+				return m, nil
 
-		// Handle key presses
-		return HandleKeyPress(m, msg)
+			case tea.KeyEnter:
+				value := m.inputText.Value()
+				m.inputText.Reset()
+				m.mode = listMode
+
+				newTask := domain.Task{
+					Title:       value,
+					IsCompleted: false,
+					DueDate:     time.Now(),
+					ListId:      "develop",
+					Priority:    0, // no prio
+					// Description: "New task description",
+					// DueDate: time.Time{}, // no date
+					Index: -1, // not on the list yet
+				}
+				m.taskService.AddTask(newTask)
+				insCmd := m.listModel.InsertItem(-1, listItem{task: newTask})
+
+				return m, insCmd
+
+			}
+
+		// We handle errors just like any other message
+		case errMsg:
+			m.err = msg
+			return m, nil
+		}
+
+		m.inputText, cmd = m.inputText.Update(msg)
+		return m, cmd
+
+	case dateInputMode:
+		var cmd tea.Cmd
+
+		switch msg := msg.(type) {
+		case tea.KeyMsg:
+			switch msg.Type {
+			case tea.KeyCtrlC, tea.KeyEsc:
+				m.inputText.Reset()
+				m.mode = listMode
+				return m, nil
+
+			case tea.KeyEnter:
+				// value := m.inputText.Value()
+				m.inputText.Reset()
+				m.mode = listMode
+
+				// TODO: implementation of card editing missing in reminders-cli
+				// newTask := domain.Task{
+				// 	Title:       value,
+				// 	IsCompleted: false,
+				// 	ListId:      "develop",
+				// 	Priority:    0, // no prio
+				// 	// Description: "New task description",
+				// 	// DueDate: time.Time{}, // no date
+				// 	Index: -1, // not on the list yet
+				// }
+				// m.taskService.AddTask(newTask)
+				// insCmd := m.listModel.InsertItem(-1, listItem{task: newTask})
+
+				// return m, insCmd
+				return m, nil
+
+			}
+
+		// We handle errors just like any other message
+		case errMsg:
+			m.err = msg
+			return m, nil
+		}
+
+		m.inputText, cmd = m.inputText.Update(msg)
+		return m, cmd
 
 	}
-
-	// This will also call our delegate's update function.
-	newListModel, cmd := m.listModel.Update(msg)
-	m.listModel = newListModel
-	cmds = append(cmds, cmd)
-
-	return m, tea.Batch(cmds...)
-
+	return m, nil
 }
 
 func (m model) View() string {
@@ -72,7 +176,26 @@ func (m model) View() string {
 		return "Error: " + m.err.Error()
 	}
 
-	return appStyle.Render(m.listModel.View())
+	switch m.mode {
+	case listMode:
+		// return m.listView.View()
+		return appStyle.Render(m.listModel.View())
+	case textInputMode:
+
+		return fmt.Sprintf(
+			"Insert new card\n\n%s\n\n%s",
+			m.inputText.View(),
+			"(esc to quit)",
+		) + "\n"
+
+	case dateInputMode:
+		return fmt.Sprintf(
+			"Insert new card due date\n\n%s\n\n%s",
+			m.inputText.View(),
+			"(esc to quit)",
+		) + "\n"
+	}
+	return ""
 }
 
 func (b BubbleTeaApp) Run() error {

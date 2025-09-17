bd2949fae6d2a6e84d00dc927f1787d2bfb05da4
Florian Bellmann <flojuesten@gmail.com>
2025-09-17 22:20:47 +0200

    creation time on task

--- lazytask/domain/task.py
+++ lazytask/domain/task.py
@@ -2,12 +2,15 @@ from dataclasses import dataclass, field
 from typing import Optional, List
 import datetime
 
+
 @dataclass
 class Task:
     id: str
     title: str
     completed: bool = False
     due_date: Optional[datetime.date] = None
+    creation_date: Optional[datetime.datetime] = None
+    list_name: Optional[str] = None
     description: Optional[str] = None
     tags: List[str] = field(default_factory=list)
     priority: Optional[int] = None

ae5c6d9da7e89b2c905c1224500b66b8e4ac4a8d
Florian Bellmann <flojuesten@gmail.com>
2025-09-25 13:16:32 +0200

    linting

--- lazytask/presentation/help_screen.py
+++ lazytask/presentation/help_screen.py
@@ -1,6 +1,8 @@
 from textual.app import ComposeResult
 from textual.screen import ModalScreen
 from textual.widgets import Static
+from textual.binding import Binding
+from typing import cast, Tuple
 
 
 class HelpScreen(ModalScreen):
@@ -13,8 +15,11 @@ class HelpScreen(ModalScreen):
             bindings = self.parent.BINDINGS
             text = []
             for binding in bindings:
-                key, action, description = binding
-                text.append(f"{key.upper()}: {description}")
+                if isinstance(binding, Binding):
+                    text.append(f"{binding.key.upper()}: {binding.description}")
+                else:
+                    key, action, description = cast(Tuple[str, str, str], binding)
+                    text.append(f"{key.upper()}: {description}")
             yield Static("\n".join(text), id="help-text")
         else:
             yield Static("No help available.", id="help-text")
